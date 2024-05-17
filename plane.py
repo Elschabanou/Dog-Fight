4f0917876748fe31e91f83e55566e08f730400fe
Elschabanou <felix@schabana.de>
2024-05-17 09:33:46 +0200

    new plane class

---
+++ plane.py
@@ -0,0 +1,22 @@
+class Plane():
+
+    def __init__(self, speed, hp, damage):
+        self._speed = speed
+        self._hp = hp
+        self._damage = damage
+
+    def shoot(self):
+        pass
+
+    def move(self):
+        pass
+
+    def recieveDamage(self, damage):
+        pass
+
+    def getDestroyed(self):
+
+        if self._hp <= 0:
+            pass
+        else: 
+            pass
