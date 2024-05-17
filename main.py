a09d0e6ed2bec3baf536bb9eb03de3543071236e
Elschabanou <felix@schabana.de>
2024-05-17 18:58:47 +0200

    shoot function when pressing buttons implemented

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ class DogFight():
         self._screen = pygame.display.set_mode((500, 500))
         self._clock = pygame.time.Clock()
         self._running = True
+        # create the two players
+        self._player1 = Plane(self,300,30,0,0,0,1)
+        self._player2 = Plane(self,300,30,0,0,0,2)
 
-    def draw(self):
-        self._screen.fill("purple")
+
+    def draw_player(self):
+        pass
+
+    def draw_background(self):
+        # later use image of clouds
+        self._screen.fill("lightblue")
         
     def run(self):
         
@@ -25,10 +33,18 @@ class DogFight():
 
                 if event.type == pygame.QUIT:
                     self._running = False
+
+            # gets a list of all the buttons that are pressed
+            self._keys = pygame.key.get_pressed()
+            if self._keys[pygame.K_SPACE]: # Space to shoot for player 1
+                self._player1.shoot()
+            if self._keys[pygame.K_l]: # "l" to shoot for player 2
+                self._player2.shoot()
             
-            self._screen.fill("lightblue")
+            
+            self.draw_background()
             pygame.display.flip()
-            self._clock.tick(60)
+            self._clock.tick(30) #Framerate
         
         pygame.quit()
 
--- plane.py
+++ plane.py
@@ -1,15 +1,17 @@
 class Plane():
 
-    def __init__(self, speed, hp, damage, xPos, yPos, heading):
+    def __init__(self, speed, hp, damage, xPos, yPos, heading, player):
         self._speed = speed
         self._hp = hp
         self._damage = damage
         self._xPos = xPos
         self._yPos = yPos
+        self._player = player # key that player has to press to shoot
         self._heading = heading     #describes the direction of the Plane in the field  | 1 upwards;  2 diagonal right upwards; 3 right; 4 ...
 
     def shoot(self):
-        pass
+        print("shoot")
+        
 
     def move(self):
         #funciton to fly upwards, the yPos changes with every tick by speed 
