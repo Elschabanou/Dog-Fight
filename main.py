092f1fb52b3ff37342bcc1a03c62b762d598f190
Elschabanou <felix@schabana.de>
2024-05-17 09:25:56 +0200

    setup 2

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import tkinter as tk
 print("Hallo Dog-Fight")
+
+
+
+
+class DogFight():
+    
+    def __init__(self):
+        self._root = tk.Tk()
+        self._root.title("Hallo Dog-Fight")
+        self._root.mainloop()
+
+
+
+if __name__ == "__main__":
+    DogFight()
